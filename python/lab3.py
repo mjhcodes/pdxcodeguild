44197398adbbfa6b53270a0c464c55827f74944c
Matthew J. Hughes <mjhughes707@gmail.com>
2019-10-03 22:08:01 -0700

    separated the print portion into its own function

--- python/lab3.py
+++ python/lab3.py
@@ -8,25 +8,34 @@ def getQualifier(remainder):
     qualifier = ""
   return qualifier
 
-def getGrade(student_grade, qualifier):
+def getGrade(percentage):
   """determines letter grade"""
-  if student_grade >= 90:
-    print("A" + qualifier)
-  elif student_grade >= 80:
-    print("B" + qualifier)
-  elif student_grade >= 70:
-    print("C" + qualifier)
-  elif student_grade >= 60:
-    print("D" + qualifier)
+  if percentage >= 90:
+    grade = "A"
+  elif percentage >= 80:
+    grade = "B"
+  elif percentage >= 70:
+    grade = "C"
+  elif percentage >= 60:
+    grade = "D"
   else:
-    print("F")
+    grade = "F"
+  return grade
+
+def printGrade(grade, qualifier):
+  """prints letter grade with qualifier, if applicable"""
+  if grade == "F":
+    print(f"Student Grade: {grade}")
+  else:
+    print(f"Student Grade: {grade}{qualifier}")
 
 def main():
   """main program loop"""
   while True:
-    student_grade = int(input("Enter a number representing the grade: "))
-    qualifier = getQualifier(student_grade % 10)
-    getGrade(student_grade, qualifier)
+    percentage = int(input("Enter the grade percentage: "))
+    qualifier = getQualifier(percentage % 10)
+    grade = getGrade(percentage)
+    printGrade(grade, qualifier)
     repeat = input("Would you like to enter another grade? (y or n) ").lower()
     if repeat.startswith("n"):
       break
