5e44d3efe8664910e5859e5a4dd495f31f3351ce
Matthew J. Hughes <mjhughes707@gmail.com>
2019-10-04 20:26:04 -0700

    completed Lab 6 - Version 2

--- python/lab6.py
+++ python/lab6.py
@@ -1,12 +1,17 @@
 import random, string
 
-def password_generator():
-  """creates a ten character password"""
+def password_generator(n):
+  """creates a random ten character password"""
   i = 0
   password = ""
-  while i < 10:
+  while i < n:
     password += random.choice(string.ascii_letters + string.digits)
     i += 1
   return password
 
-print(password_generator())
+def main():
+  """accepts custom password length and prints randomly generated password accordingly"""
+  length = int(input("Enter the desired password length: "))
+  print(password_generator(length))
+
+main()
