86b7a3aac41cd5533aac0abfb8030f7bb0f8e4be
Matthew J. Hughes <mjhughes707@gmail.com>
2019-10-04 19:39:58 -0700

    completed Lab 4 - Version 1

---
+++ python/lab4.py
@@ -0,0 +1,9 @@
+import random
+
+predictions = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+
+print("\nWelcome to Magic 8 Ball!\n")
+
+user_question = input("What is your question? ")
+
+print(f"\n{random.choice(predictions)}\n")
