c4eed0f2346c867e6668e3c73d30acc695a67513
Matthew J. Hughes <mjhughes707@gmail.com>
2019-10-08 21:20:17 -0700

    completed Lab 10 - Version 2

--- python/lab10.py
+++ python/lab10.py
@@ -1,15 +1,27 @@
 # Lab 10 - Average Numbers
 
-def sum_nums(nums):
+def get_nums():
+  """prompts user for numbers and returns complete list of numbers when finished"""
+  num_list = []
+  while True:
+    new_num = input("Enter a number or type 'done': ")
+    try:
+      new_num = float(new_num)
+      num_list.append(new_num)
+    except:
+      break
+  return num_list
+
+def sum_nums(num_list):
   """accepts list of numbers and returns sum"""
   sum_total = 0
-  for num in nums:
+  for num in num_list:
     sum_total += num
   return sum_total
 
-def get_length(nums):
+def get_length(num_list):
   """accepts list of numbers and returns length"""
-  list_length = len(nums)
+  list_length = len(num_list)
   return list_length
 
 def average_nums(sum_total, list_length):
@@ -18,10 +30,12 @@ def average_nums(sum_total, list_length):
   num_average = "{0:.4f}".format(num_average)
   return num_average
 
-def main(nums):
-  sum_total = sum_nums(nums)
-  list_length = get_length(nums)
+def main():
+  """gathers numbers from user, sums total, determines length, calculates average and prints result"""
+  num_list = get_nums()
+  sum_total = sum_nums(num_list)
+  list_length = get_length(num_list)
   num_average = average_nums(sum_total, list_length)
-  print(num_average)
+  print(f'\nAverage: {num_average}\n')
 
-main([5, 0, 8, 3, 4, 1, 6])
+main()
