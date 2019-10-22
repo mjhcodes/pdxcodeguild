e959d8550b81610d4012d9a37ea3ae260c3a1db3
Matthew J. Hughes <mjhughes707@gmail.com>
2019-10-21 21:18:39 -0700

    working through Lab 24 - Version 2

--- python/lab24.py
+++ python/lab24.py
@@ -1,5 +1,8 @@
 # Lab 24 - Rain Data
 
+import datetime
+import matplotlib.pyplot as plt
+
 def open_file():
   """opens the file and returns its contents as a list separated by each row"""
   with open('lab24_ankeny_rain.txt', 'r') as data:
@@ -36,11 +39,32 @@ def get_merged_data(dates, daily_totals):
   merged_data = list(zip(dates, daily_totals))
   return merged_data
 
+def calculate_mean(daily_totals):
+  """calculates the mean of the data by dividing the total sum by the length of the list"""
+  sum_total = sum(daily_totals)
+  return sum_total / len(daily_totals)
+
+def calculate_variance(mean, daily_totals):
+  """calculates the variance by accepting the mean; then, for each number, subtracts the mean and squares the result; finally, returns the average of those squared differences"""
+  sq_diffs = [((num - mean) ** 2) for num in daily_totals]
+  sum_sq_diff = sum(sq_diffs)
+  return sum_sq_diff / len(sq_diffs)
+
+def get_max_rain_date(merged_data):
+  """iterates through the tuples of merged rain data and returns the date with the highest rain total"""
+  rain_totals = [date[1] for date in merged_data]
+  max_rain_total = max(rain_totals)
+  max_rain_date = [date[0] for date in merged_data if date[1] == max_rain_total]
+  return max_rain_date
+
 def main():
   data = open_file()
   dates = get_dates(data)
   daily_totals = get_daily_totals(data)
   merged_data = get_merged_data(dates, daily_totals)
-  print(merged_data)
+  mean = calculate_mean(daily_totals)
+  variance = calculate_variance(mean, daily_totals)
+  max_rain_date = get_max_rain_date(merged_data)
+  print(max_rain_date)
 
 main()
