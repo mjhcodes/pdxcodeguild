d2bedd0719eeb9cce7c128a98299e64e86ca8815
Matthew J. Hughes <mjhughes707@gmail.com>
2019-10-21 19:50:43 -0700

    completed Lab 25 - Version 2

--- python/lab25.py
+++ python/lab25.py
@@ -1,8 +1,9 @@
 # Lab 25 - ATM
 
 class ATM:
-  def __init__(self, balance = 0):
+  def __init__(self, balance = 0, transactions = []):
     self.balance = balance
+    self.transactions = transactions
   
   def check_balance(self):
     """returns the account balance"""
@@ -11,6 +12,7 @@ class ATM:
   def deposit(self, amount):
     """deposits the given amount in the account"""
     self.balance += amount
+    self.transactions.append(f'user deposited ${amount}')
 
   def check_withdrawal(self, amount):
     """returns true if the withdrawn amount won't put the account in the negative"""
@@ -20,4 +22,15 @@ class ATM:
   def withdraw(self, amount):
     """withdraws the amount from the account and returns it"""
     self.balance -= amount
+    self.transactions.append(f'user withdrew ${amount}')
     return self.balance
+
+  def print_transactions(self):
+    """prints the list of transactions"""
+    print(self.transactions)
+
+
+
+
+
+
