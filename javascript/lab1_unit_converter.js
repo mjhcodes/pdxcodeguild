17f89b24585fe7b68150319fceb7195e9c29251d
Matthew J. Hughes <mjhughes707@gmail.com>
2019-11-19 18:26:03 -0800

    Completed Lab 1 - Unit Converter - Versions 2 & 3

--- javascript/lab1_unit_converter.js
+++ javascript/lab1_unit_converter.js
@@ -1,13 +1,23 @@
 const readline = require("readline-sync");
 
-function convertToMeters(feet) {
-  return feet * 0.3048;
+const conversionTable = {
+  "ft": 0.3048,
+  "mi": 1609.34,
+  "m": 1,
+  "km": 1000,
+  "yard": 0.9144,
+  "inch": 0.0254,
+}
+
+function convertToMeters(distance, unit) {
+  return distance * conversionTable[unit];
 }
 
 function main() {
-  let userInput = parseInt(readline.question("What is the distance in feet? "));
-  let meters = convertToMeters(userInput);
-  console.log(`${userInput} ft is ${meters.toFixed(4)} m`);
+  let userDistance = parseInt(readline.question("What is the distance? "));
+  let userUnit = readline.question("What are the units? ");
+  let meters = convertToMeters(userDistance, userUnit);
+  console.log(`${userDistance} ${userUnit} is ${meters.toFixed(4)} m`);
 }
 
 main()
