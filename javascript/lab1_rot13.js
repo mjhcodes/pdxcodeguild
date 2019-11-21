ce1112ba0b810358b78d51117fba90680ce3fdea
Matthew J. Hughes <mjhughes707@gmail.com>
2019-11-20 20:14:25 -0800

    Completed Lab 1 - ROT13 - Version 2

--- javascript/lab1_rot13.js
+++ javascript/lab1_rot13.js
@@ -1,20 +1,44 @@
 const readline = require("readline-sync");
 
-function getEncryptedString(alphabet, userString) {
-  let encryptedString = ""
-  for (let i = 0; i < userString.length; i++) {
-    let n = alphabet.indexOf(userString[i]);
-    n < 13 ? n += 13 : n -= 13;
-    encryptedString = encryptedString.concat(alphabet[n]);
+function reverse(lowerLetters, upperLetters, rotation) {
+  lowerLetters = lowerLetters.split("").reverse().join("");
+  upperLetters = upperLetters.split("").reverse().join("");
+  rotation = Math.abs(rotation);
+  return lowerLetters, upperLetters, rotation;
+}
+
+function getEncrypedString(rotation, userString, lowerLetters, upperLetters) {
+  let encryptedString = '';
+  for (i = 0; i < userString.length; i++) {
+    index = lowerLetters.indexOf(userString[i]);   
+
+    if (index !== -1)
+      encryptedString += lowerLetters[(index + rotation) % 26];
+    else if (index === -1) {
+      index = upperLetters.indexOf(userString[i]);
+      if (index === -1)
+        encryptedString += userString[i];
+      else
+        encryptedString += upperLetters[(index + rotation) % 26];
+    }
   }
-  return encryptedString
+  return encryptedString;
 }
 
 function main() {
-  const alphabet = "abcdefghijklmnopqrstuvwxyz";
-  let userString = readline.question("\nenter a string: ").toLowerCase();
-  encryptedString = getEncryptedString(alphabet, userString);
-  console.log(`${encryptedString}\n`);
+  let rotation = parseInt(readline.question("\nenter the rotation: "));
+  const userString = readline.question("enter a string: ");
+  
+  let lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+  let upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  
+  if (rotation < 0) {
+    lowerLetters, upperLetters, rotation = reverse(lowerLetters, upperLetters, rotation);
+  }
+  
+  encryptedString = getEncrypedString(rotation, userString, lowerLetters, upperLetters);
+  
+  console.log(`\n${encryptedString}\n`);
 }
 
 main()
